2992e38c06faba1c1183633016d400978631db4e
S.j. Sakib <sjsakib.bd@gmail.com>
2017-11-19 15:45:09 +0600

    added pipeline

--- slist/pipelines.py
+++ slist/pipelines.py
@@ -1,11 +1,21 @@
-# -*- coding: utf-8 -*-
-
-# Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
+import requests
 
 
 class SlistPipeline(object):
+
+    def open_spider(self, spider):
+        self.data = {}
+
     def process_item(self, item, spider):
+        user = item['id']
+        if user not in self.data:
+            self.data[user] = {'failed': [], 'judges': {}}
+        if item['status'] == 'FAILED':
+            self.data[user]['failed'].append(item['judge'])
+        else:
+            self.data[user]['judges'][item['judge']] = item['lst']
+
         return item
+
+    def close_spider(self, spider):
+        requests.post(spider.settings.get('POST_URL'), json=self.data)
